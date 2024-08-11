6a86d3f02a7fb636daaeab3a135a02c7d265b2d8
Johan Söderlund <johan.soderlund96@gmail.com>
2024-08-11 04:21:11 +0200

    implemented typewriter effect for h2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HeaderImage from "../assets/header.jpg";
-import ScrollDown from "./ScrollDown"; // Import the ScrollDown component
+import ScrollDown from "./ScrollDown";
 import "../styles/Header.css";
 
 const Header = () => {
+  const phrases = [
+    "Web Developer",
+    "UI/UX Interested",
+    "Media Server Enthusiast",
+  ];
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const [displayedText, setDisplayedText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  useEffect(() => {
+    let timer;
+    const currentPhrase = phrases[currentPhraseIndex];
+
+    if (!isDeleting && displayedText === currentPhrase) {
+      timer = setTimeout(() => setIsDeleting(true), 5000);
+    } else if (isDeleting && displayedText === "") {
+      setIsDeleting(false);
+      setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
+    } else {
+      timer = setTimeout(
+        () => {
+          setDisplayedText((prev) =>
+            isDeleting
+              ? prev.slice(0, -1)
+              : currentPhrase.slice(0, prev.length + 1)
+          );
+        },
+        isDeleting ? 40 : 60
+      );
+    }
+
+    return () => clearTimeout(timer);
+  }, [displayedText, currentPhraseIndex, isDeleting]);
+
   const parallaxStyle = {
     backgroundImage: `url(${HeaderImage})`,
     backgroundAttachment: "fixed",
@@ -16,7 +50,9 @@ const Header = () => {
     <header className="parallax" style={parallaxStyle}>
       <div className="content">
         <h1>Johan Söderlund</h1>
-        <p>Web developer</p>
+        <p className="typewriterContainer">
+          <span className="typewriter">{displayedText}</span>
+        </p>
       </div>
       <ScrollDown />
     </header>
