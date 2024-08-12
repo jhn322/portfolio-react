e770dba2f982da4523299e4afb35584a5483207d
Johan Söderlund <johan.soderlund96@gmail.com>
2024-08-12 21:56:48 +0200

    dynamic import of emailJS to resolve error module

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,9 @@
 import { useState } from "react";
-import emailjs from "@emailjs/browser";
 import { RxExternalLink } from "react-icons/rx";
 import "../styles/Contact.css";
 
 const handleEmailClick = () => {
-  window.location.href = `mailto:"johan.soderlund96@gmail.com"`;
+  window.location.href = `mailto:johan.soderlund96@gmail.com`;
 };
 
 const Contact = () => {
@@ -22,7 +21,7 @@ const Contact = () => {
     });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     if (!formData.name || !formData.email || !formData.message) {
@@ -30,26 +29,26 @@ const Contact = () => {
       return;
     }
 
-    const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
-    const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
-    const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
+    try {
+      const emailjs = await import("@emailjs/browser");
 
-    const emailParams = {
-      from_name: formData.name,
-      to_name: "Johan",
-      message: formData.message,
-      from_email: formData.email,
-    };
+      const serviceId = import.meta.env.VITE_EMAILJS_SERVICE_ID;
+      const templateId = import.meta.env.VITE_EMAILJS_TEMPLATE_ID;
+      const publicKey = import.meta.env.VITE_EMAILJS_PUBLIC_KEY;
 
-    emailjs
-      .send(serviceId, templateId, emailParams, publicKey)
-      .then((response) => {
-        alert("Message sent successfully!");
-      })
-      .catch((error) => {
-        alert("Failed to send message, please try again.");
-        console.error("EmailJS error:", error);
-      });
+      const emailParams = {
+        from_name: formData.name,
+        to_name: "Johan",
+        message: formData.message,
+        from_email: formData.email,
+      };
+
+      await emailjs.send(serviceId, templateId, emailParams, publicKey);
+      alert("Message sent successfully!");
+    } catch (error) {
+      alert("Failed to send message, please try again.");
+      console.error("EmailJS error:", error);
+    }
 
     setFormData({
       name: "",
@@ -68,7 +67,7 @@ const Contact = () => {
         <h3 className="infoTitle">My Information</h3>
         <ul className="infoList">
           <li className="infoItem">
-            <span className="label">Adress:</span>
+            <span className="label">Address:</span>
             <span className="value">Umeå, Sweden</span>
           </li>
           <li className="infoItem">
