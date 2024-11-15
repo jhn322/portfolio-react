f4a740f6e0e868793a5444bc835f6d25a9ff8898
Johan Söderlund <johan.soderlund96@gmail.com>
2024-11-15 14:35:58 +0100

    resolved conflicts and changed header phrases

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import About from "./components/About";
-import Skills from "./components/Skills";
-import Experience from "./components/Experience";
-import FeaturedProjects from "./components/FeaturedProjects";
-import OtherProjects from "./components/OtherProjects";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
-import ScrollUp from "./components/ScrollUp";
-import Icons from "./components/Icons";
-// import CircleBg from "./components/LottieCircle";
-
-function App() {
-  return (
-    <div className="App">
-      {/* <CircleBg /> */}
-      <Navbar />
-      <Header />
-      <main>
-        <About />
-        <Skills />
-        <Experience />
-        <FeaturedProjects />
-        <OtherProjects />
-        <Contact />
-        <Footer />
-        <ScrollUp />
-        <Icons />
-      </main>
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from "react";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
@@ -68,4 +30,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 4f655f661172c1687ca6a3c63951be4057c0de27
